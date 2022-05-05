d9b810eaf09256af55aa17216288f9cf24515d99
unknown <dasha.danilchenko1990@gmail.com>
2022-05-05 17:55:40 +0300

    add features

--- HW_9/script/script.js
+++ HW_9/script/script.js
@@ -8,3 +8,14 @@ let close = document.querySelector (".cross")
 close.addEventListener ('click', function (){
     popup.classList.remove("active_popup")
 })
+
+home = document.getElementById ("homework_btn")
+page = document.getElementById ("page")
+home.addEventListener ('click', function (){
+    page.classList.add("page_activ") 
+})
+
+closeA = document.querySelector (".attendabce_btn")
+closeA.addEventListener ('click', function (){
+    page.classList.remove("page_activ")
+})
