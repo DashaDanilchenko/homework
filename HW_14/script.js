bc0051dcde03f2b0e5229ad486a4af58b6bfd26c
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-15 11:03:21 +0300

    task 5

--- HW_14/script.js
+++ HW_14/script.js
@@ -44,3 +44,14 @@ function popShift(arr) {
   }
   
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+
+// 5
+
+// We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+arr.splice(1, 4)
+
+console.log(arr);
