18ea119bd4e8b59bb8c93dab418b42db0e23c92b
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-15 16:54:11 +0300

    task 15

--- HW_14/script.js
+++ HW_14/script.js
@@ -204,3 +204,26 @@ let userActivity = {
 userActivity.data.online = 45;
 
 console.log(userActivity);
+
+
+//15
+// We've defined a function, checkInventory, which receives a scanned item as an argument.
+// Return the current value of the scannedItem key in the foods object.
+// You can assume that only valid keys will be provided as an argument to checkInventory.
+
+let vegetables = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  
+return vegetables[scannedItem]
+  
+}
+
+console.log(checkInventory("apples"));
