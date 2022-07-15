76df9a800ff6d7bbcd38be7e6f6fc7bb45d96a48
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-15 13:09:07 +0300

    task 10

--- HW_14/script.js
+++ HW_14/script.js
@@ -115,3 +115,18 @@ function spreadOut() {
   }
   
   console.log(spreadOut());
+
+
+// 10
+// indexOf() can be incredibly useful for quickly checking for the presence of an element on an array.
+// We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array,
+// and false if it does not.
+
+
+function quickCheck(arr, elem) {
+    
+    if (arr.indexOf(elem) === -1) {return false
+    } else {return true}
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
