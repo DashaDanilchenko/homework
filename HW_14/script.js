60b38710e7a9698c9b37d9a3c2161728c966caee
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-15 16:20:40 +0300

    task 13

--- HW_14/script.js
+++ HW_14/script.js
@@ -169,3 +169,21 @@ let myNestedArray = [
  
 ];
 console.log(myNestedArray[0][0][0][0][0])
+
+
+//13
+// Bracket notation is required if your property has a space in it or if you want to use a variable to name the property.
+// In the above case, the property is enclosed in quotes to denote it as a string and will be added exactly as shown. Without quotes,
+// it will be evaluated as a variable and the name of the property will be whatever value the variable is. Here's an example with a variable:
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods.bananas = 13;
+foods.grapes  = 35;
+foods.strawberries  = 27;
+
+console.log(foods);
