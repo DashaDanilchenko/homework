da9cc0c9fbb17b796c374403394e7e33b8f0c1fa
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-15 16:13:51 +0300

    task 12

--- HW_14/script.js
+++ HW_14/script.js
@@ -150,3 +150,22 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+
+// 12
+// We have defined a variable, myNestedArray, set equal to an array.
+// Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements,
+// so that it has exactly five levels of depth (remember, the outer-most array is level 1).
+// Somewhere on the third level, include the string deep, on the fourth level, include the string deeper,
+// and on the fifth level, include the string deepest.
+
+let myNestedArray = [
+  
+  [['deep',['deeper',['deepest']]],'unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array'],
+  ['mutate', 1327.98, 'splice', 'slice', 'push'],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+ 
+];
+console.log(myNestedArray[0][0][0][0][0])
