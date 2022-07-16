9d5bd284c0c13ec9178f0e66a0006629c0e9bec1
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-16 20:07:14 +0300

    task 19

--- HW_14/script.js
+++ HW_14/script.js
@@ -309,3 +309,34 @@ for (let  user in usersObj) {
 return quantityTrue 
 }
 console.log(countOnline(users));
+
+
+//19
+// Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument
+
+users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  
+return Object.keys(obj)
+  
+}
+
+console.log(getArrayOfUsers(users));
