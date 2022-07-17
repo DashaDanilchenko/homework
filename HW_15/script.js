64668c31c10899d52b16ea614ddbd142b8c7d40a
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-17 18:17:24 +0300

    task 4

--- HW_15/script.js
+++ HW_15/script.js
@@ -31,3 +31,14 @@ function factorialize(num) {
   }
   
   console.log(factorialize(5));
+
+
+//4
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
+function findLongestWordLength(str) {
+    return (str.split(' ').reduce((a, b) => (b.length > a.length) ? b : a)).length
+    
+    }
+    console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
