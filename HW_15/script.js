541e3570bfad8b0f9d86d5c826354df90b5e1435
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-18 15:34:52 +0300

    task 9

--- HW_15/script.js
+++ HW_15/script.js
@@ -123,3 +123,14 @@ return str
 
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 10));
 
+
+// 9
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'.
+// This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined
+
+function findElement(arr, func) {
+  
+  return  arr.filter(func)[0]
+   
+  }
+  console.log(findElement([1, 2, 3, 9], num => num % 2 === 0));
