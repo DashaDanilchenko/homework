a79869311b9b9fa4ab25487f763d93d9bce78dd7
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-24 15:45:53 +0300

    added image_puppy

--- HW_16/script.js
+++ HW_16/script.js
@@ -28,3 +28,21 @@ list.append(liTree)
 
 // put that list into the above wrapper
 shell.append(list)
+
+// create an image
+const image = document.createElement('img')
+
+// set the source to an image
+image.src = "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/3/22/0/shutterstock_national-puppy-day-301891139.jpg.rend.hgtvcom.966.725.suffix/1521744692471.jpeg"
+
+// set the width to 250
+image.style.width = '250px'
+
+// add a class of cute
+image.classList.add('cute')
+
+// add an alt of Cute Puppy
+image.alt = "Cute Puppy"
+
+// Append that image to the wrapper
+shell.append(image)
