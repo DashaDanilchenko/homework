a30d57e2cb0287ec060cb6abf60e5fe5f62e17f4
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-24 12:53:58 +0300

    create div_shell

---
+++ HW_16/script.js
@@ -0,0 +1,8 @@
+// Make a div
+const shell = document.createElement('div')
+
+// add a class of wrapper to it
+shell.classList.add('classShell')
+
+// put it into the body
+document.body.prepend(shell)
