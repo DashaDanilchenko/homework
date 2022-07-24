ea80ee7c3b384731772b0a9abb3eb368e91a8f5d
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-24 13:36:35 +0300

    create ul

--- HW_16/script.js
+++ HW_16/script.js
@@ -6,3 +6,25 @@ shell.classList.add('classShell')
 
 // put it into the body
 document.body.prepend(shell)
+
+// make an unordered list
+const list = document.createElement('ul')
+
+// add three list items with the words "one, two, three" in them
+const liOne = document.createElement('li')
+const textOne = document.createTextNode('One')
+liOne.append(textOne)
+list.append(liOne)
+
+const liTwo = document.createElement('li')
+const textTwo = document.createTextNode('Two')
+liTwo.append(textTwo)
+list.append(liTwo)
+
+const liTree = document.createElement('li')
+const textTree = document.createTextNode('Tree')
+liTree.append(textTree)
+list.append(liTree)
+
+// put that list into the above wrapper
+shell.append(list)
