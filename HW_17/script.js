3ebaa0ff660d713e6dd1e3e368f14b6b50d191e1
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-07-27 12:37:41 +0300

    create a function that inserts the selected photo into the modal window

--- HW_17/script.js
+++ HW_17/script.js
@@ -4,21 +4,29 @@ wind.classList.add('wind')
 document.body.prepend(wind)
 
 const arrData = [
-    {   src: 'https://klike.net/uploads/posts/2020-08/1597127429_5.jpg',
-        alt: 'alt',
-        text: 'text',
+    {   src: 'https://desna-buvr.gov.ua/wp-content/uploads/2020/09/photo_2020-07-12_21-14-37.jpg',
+        alt: 'Desna',
+        info: `<span>Length - 1130 kl.</span> <br>
+            <span>Pool - 88 900 kl².</span> <br>
+            <span>Water consumption - 360 м³/ы (at the mouth)</span>`,
     },
-    {   src: 'https://klike.net/uploads/posts/2020-08/1597127429_5.jpg',
-        alt: 'alt',
-        text: 'text',
+    {   src: 'https://aborigen.org/images/reki/reka-vorskla1.jpg',
+        alt: 'Vorskla',
+        info:  `<span>Length - 	464 kl.</span> <br>
+            <span>Pool - 14 700 kl².</span> <br>
+            <span>Water consumption - 36,4 м³/ы (at the mouth)</span>`,
     },
-    {   src: 'https://klike.net/uploads/posts/2020-08/1597127429_5.jpg',
-        alt: 'alt',
-        text: 'text',
+    {   src: 'https://photographers.ua/thumbnails/pictures/2894/800x_pered_vorozhboym.jpg',
+        alt: 'Psel',
+        info:  `<span>Length - 	717 kl.</span> <br>
+            <span>Pool - 22 800 kl².</span> <br>
+            <span>Water consumption - 55 м³/ы (at the mouth)</span>`,
     },
-    {   src: 'https://klike.net/uploads/posts/2020-08/1597127429_5.jpg',
-        alt: 'alt',
-        text: 'text',
+    {   src: 'https://img.freepik.com/free-photo/grain-transport-barge-going-up-the-tiete-river-tieteparana-waterway-drone-view_90251-1566.jpg',
+        alt: 'Ariel',
+        info: `<span>Length - 	346 kl.</span> <br>
+            <span>Pool - 9 800 kl².</span> <br>
+            <span>Water consumption - 0,41 м³/ы (at the mouth)</span>`,
     },
 ]
 
@@ -29,16 +37,24 @@ function createCard(card) {
     photo.classList.add('photo')
     photo.innerHTML = 
     `<img src="${card.src}" alt="${card.alt}">
-    <p>${card.text}</p>`
+    <h3>${card.alt}</h3>
+    <p>${card.info}</p>`
     wind.append(photo)
-    photo.addEventListener('click', watch)
+    photo.addEventListener('mouseover', watchPhoto)
+    photo.addEventListener('mouseout', watchWind)
      return photo
 }
 
-function watch(e) {
-    console.log(e.currentTarget)
+// create a function that inserts the selected photo into the modal window
+
+function watchPhoto(e) {
+    document.body.prepend(this)
+    wind.classList.add('opacity')
+    this.classList.add('position')
 }
 
-
-// create a modal window
-// create a function that inserts the selected photo into the modal window
+function watchWind (e) {
+    wind.append(this)
+    wind.classList.remove('opacity')
+    this.classList.remove('position')
+}
